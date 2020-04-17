b9336494e4fa19ef3003f19e20080f6d079051bd
Szabó Zoltán <szabo.zoltan@aki.gov.hu>
2020-04-17 10:19:57 +0200

    0.0.3 Database table change

--- webc.sql
+++ webc.sql
@@ -3,6 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Gép: 127.0.0.1
+-- Létrehozás ideje: 2020. Ápr 17. 10:18
 -- Kiszolgáló verziója: 10.4.11-MariaDB
 -- PHP verzió: 7.4.3
 
@@ -18,8 +19,10 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Adatbázis: `test1`
+-- Adatbázis: `webc`
 --
+CREATE DATABASE IF NOT EXISTS `webc` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `webc`;
 
 -- --------------------------------------------------------
 
@@ -30,6 +33,9 @@ SET time_zone = "+00:00";
 CREATE TABLE `pages` (
   `path` varchar(512) NOT NULL,
   `referer` text DEFAULT NULL,
+  `title` text DEFAULT NULL,
+  `description` text DEFAULT NULL,
+  `h1` text DEFAULT NULL,
   `download_time` timestamp NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
