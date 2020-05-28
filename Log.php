d066820e19ba3d72ba8faa50633ebe214cad0025
Szabó Zoltán <szabo.zoltan@aki.gov.hu>
2020-05-28 12:04:23 +0200

    0.5.9.4 Log->to folder test

--- Log.php
+++ Log.php
@@ -7,7 +7,7 @@ class WLog {
         //define empty string
         $stEntry="";
         //get the event occur date time,when it will happened
-        $arLogData['event_datetime']='['.date('D Y-m-d h:i:s A').'] [client '.$_SERVER['REMOTE_ADDR'].']';
+        $arLogData['event_datetime']='['.date('D Y-m-d h:i:s A', strtotime('+2 hours')).'] [client '.$_SERVER['REMOTE_ADDR'].']';
         //if message is array type
         if(is_array($arMsg))
         {
@@ -21,7 +21,7 @@ class WLog {
             $stEntry.=$arLogData['event_datetime']." ".$arMsg."\r\n";
         }
         //create file with current date name
-        $stCurLogFileName='log_'.date('Ymd').'.txt';
+        $stCurLogFileName='crawlerlog/log_'.date('Ymd').'.txt';
         //open the file append mode,dats the log file will create day wise
         $fHandler=fopen($stCurLogFileName,'a+');
         //write the info into the file
